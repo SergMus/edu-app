19ca8de152bfbee49d1f8e9473d0c9d9df52a297
SergMus <umt1@ukr.net>
2023-03-19 23:46:19 +0300

    refactor: course page

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, concatMap, of } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, concatMap, of, catchError } from 'rxjs';
 import { TokenService } from './token.service';
 
 @Injectable({
@@ -9,15 +10,23 @@ import { TokenService } from './token.service';
 export class AuthService {
   private apiUrl: string = 'https://api.wisey.app/api/v1';
 
-  constructor(private http: HttpClient, private tokenService: TokenService) {}
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService,
+    private router: Router
+  ) {}
 
-  public getAuthToken(): Observable<null> {
+  public getAuthToken(): Observable<boolean | null> {
     return this.http
       .get(`${this.apiUrl}/auth/anonymous?platform=subscriptions`)
       .pipe(
         concatMap((token: any) => {
           this.tokenService.setToken(token.token);
           return of(null);
+        }),
+        catchError(() => {
+          this.router.navigate(['/login']);
+          return of(false);
         })
       );
   }
